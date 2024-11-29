d32e985003aa822122bd4af21729216512a119a9
Sasha <55playn77@gmail.com>
2024-11-29 06:32:39 +0300

    Initial commit

---
+++ src/main/java/com/microservices/inventoryService/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.microservices.inventoryService.repository;
+
+import com.microservices.inventoryService.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
